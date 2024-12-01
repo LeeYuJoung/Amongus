f2b7f9d5e104165570dcfd30d364afaf71a571dd
LeeYuJoung <nania000822@gmail.com>
2024-12-01 22:19:05 +0900

    RoomManager

---
+++ Assets/02.Scripts/Manager/RoomManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+// Offline Scene(게임을 위한 네트워크에 접속하지 않은 씬)
+// Online Scene (게임 네트워크에 접속한 후 실제 게임을 시작하기 전 게임 대기실 씬)
+// Room Scene (플레이어들이 접속해서 게임이 준비되기를 기다리는 대기실 씬)
+public class RoomManager : NetworkRoomManager
+{
+
+}
